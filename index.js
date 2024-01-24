15324f8d06bbbd8b20d706b48dac0027e5fa59c3
Sebastian <129358561+sebastianbraeutigam@users.noreply.github.com>
2024-01-24 15:41:15 +0100

    update index.js

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather?";
 //notice: the Link https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&lang=${lang}&units=${units}
 const API_KEY = "67c0cae6ce96b1c561d6d1b1d9e96314";
 
-const city = "Elsnigk"; //searched city
-const countryCode = "de"; //country code 
 const lang = "de"; //language 
 const units = "metric"; //units of measurement, available units parameter: standard, metric and imperial ->  read more: https://openweathermap.org/current#data
 
