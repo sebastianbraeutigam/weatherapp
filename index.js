dbbbd2357c44b181aa27d0a18dd442da7432de71
Sebastian <129358561+sebastianbraeutigam@users.noreply.github.com>
2024-03-14 22:17:14 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import  API_KEY  from './config.js';
 
 const app = express();
 const port = 3000;
 
 const API_URL = "https://api.openweathermap.org/data/2.5/weather?";
 //notice: the Link https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&lang=${lang}&units=${units}
-const API_KEY = "67c0cae6ce96b1c561d6d1b1d9e96314";
+//const API_KEY = "67c0cae6ce96b1c561d6d1b1d9e96314";
+
 
 const lang = "de"; //language 
 const units = "metric"; //units of measurement, available units parameter: standard, metric and imperial ->  read more: https://openweathermap.org/current#data
